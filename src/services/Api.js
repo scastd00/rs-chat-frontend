7e2a844a292927cdb437193e095cec883a851ee8
scastd00 <scastd00@estudiantes.unileon.es>
2022-06-23 01:25:46 +0200

    Updated baseURL of api calls. Testing bi-repo configuration. DTH

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Api = axios.create({
-  baseURL: 'http://127.0.0.1/api/v1',
+  baseURL: 'https://spring-chat-backend.herokuapp.com/api/v1',
 });
 
 export default Api;
