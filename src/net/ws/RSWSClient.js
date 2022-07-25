26b34f0d907fc3a4170a85746d6d5386c5f1297a
scastd00 <scastd00@estudiantes.unileon.es>
2022-07-25 23:54:15 +0200

    Using the new custom hook

--- src/net/ws/RSWSClient.js
+++ src/net/ws/RSWSClient.js
@@ -1,15 +1,24 @@
-function RSWSClient(clientName, chatId = null) {
-  this.clientName = clientName;
-  this.chatId = chatId;
+function RSWSClient() {
+  const url = import.meta.env.PROD
+    ? 'ws://spring-chat-backend.herokuapp.com:9090'
+    : 'ws://localhost:9090';
 
-  this.getSocket = () => {
-    return this.socket;
-  };
-}
-
-RSWSClient.prototype.connect = function() {
-  const url = 'ws://localhost:9090';
   this.socket = new WebSocket(url);
 }
+
+/**
+ * Sends a message to the connected server (string or JSON).
+ *
+ * @param {string|object} message message to send.
+ */
+RSWSClient.prototype.send = function(message) {
+  if (typeof message === 'string') {
+    this.socket.send(message);
+  } else if (typeof message === 'object') {
+    this.socket.send(JSON.stringify(message));
+  } else {
+    alert('Could not send message (type must be a string or an object)');
+  }
+};
 
 export default RSWSClient;
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -4,18 +4,15 @@ import { Container, CssBaseline, Grid } from '@mui/material';
 import ChatTextBar from '../components/ChatTextBar';
 import ChatBox from '../components/ChatBox';
 import { DEFAULT_MESSAGES } from '../utils/constants';
-import ChatService from '../services/ChatService';
-import { checkResponse } from '../utils';
-import { useNavigate } from 'react-router';
-import { useDispatch, useStore } from 'react-redux';
-import RSWSClient from '../net/ws/RSWSClient'
+import { useStore } from 'react-redux';
+import { useWebSocket } from '../hooks/useWebSocket';
 
 function Chat(props) {
   const { id } = useParams();
   const [chatType, chatId] = id.split('-');
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const userState = useStore().getState().user;
+  const state = useStore().getState();
+  const userState = state.user;
+  const socket = useWebSocket().getSocket();
 
   const [totalMessages, setTotalMessages] = useState(DEFAULT_MESSAGES.reverse());
 
@@ -36,45 +33,19 @@ function Chat(props) {
   useEffect(checkUserAccessToCourseAndGroupChats, []);
 
   function sendTextMessage(textMessage) {
-    const messageData = {
-      message: textMessage,
-      metadata: {
-        chatId,
-        chatType,
-        username: userState.user.username,
-        date: new Date(),
-      },
-    };
-
-    ChatService
-      .sendTextMessage(messageData, id, userState.tokens.accessToken)
-      .catch(error => {
-        checkResponse(error, navigate, dispatch);
-        alert(error.message);
-      });
-  }
-
-  const client = new RSWSClient(userState.user.username, id);
-  client.connect();
-  const clientSocket = client.getSocket();
-
-  clientSocket.onmessage = function(message) {
-    console.log(message);
-  };
-
-  clientSocket.onopen = function() {
-    clientSocket.send(JSON.stringify({
+    socket.send({
       headers: {
-        username: 'Process WEB',
-        chatId: 'c-1',
-        dateSignIn: 484029384028409,
-        type: 'NEW_USER',
+        username: userState.user.username,
+        chatId,
+        dateSignIn: 0, // Todo: take from session of the state of the user
+        type: chatType,
+        Authentication: userState.tokens.accessToken,
       },
       body: {
         encoding: 'UTF-8',
-        content: 'Hello, I am a web client',
+        content: textMessage,
       },
-    }));
+    });
   }
 
   return (
