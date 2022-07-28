88fc5fc5fdfcdeec7441b3cb5311686749b5dfa5
scastd00 <scastd00@estudiantes.unileon.es>
2022-07-28 23:42:21 +0200

    [fix] Fixed URL of the backend of the app (DTH)

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const Api = axios.create({
   baseURL: import.meta.env.PROD
-    ? 'https://spring-chat-backend.herokuapp.com/api/v1'
+    ? 'https://rschat-ws-back.herokuapp.com/api/v1'
     : 'http://127.0.0.1:8080/api/v1',
 });
 
