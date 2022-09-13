faa6183ccc0a0bf3ccd6929e7b9c09a65ff2f623
scastd00 <scastd00@estudiantes.unileon.es>
2022-09-13 14:16:05 +0200

    Icons Administration buttons

--- src/pages/Administration.jsx
+++ src/pages/Administration.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { Button, Container, CssBaseline, Grid } from '@mui/material';
 import { useNavDis } from '../hooks/useNavDis';
+import BookIcon from '@mui/icons-material/Book';
+import SchoolIcon from '@mui/icons-material/School';
+import GroupsIcon from '@mui/icons-material/Groups';
+import PersonIcon from '@mui/icons-material/Person';
+import AnalyticsIcon from '@mui/icons-material/Analytics';
 
 function Administration() {
   const [navigate] = useNavDis();
+  const items = [
+    { name: 'degrees', icon: SchoolIcon },
+    { name: 'subjects', icon: BookIcon },
+    { name: 'groups', icon: GroupsIcon },
+    { name: 'users', icon: PersonIcon },
+    { name: 'statistics', icon: AnalyticsIcon },
+  ]
 
   return (
     <Container sx={{ py: 3 }} component='main'>
@@ -11,16 +23,16 @@ function Administration() {
 
       <Grid container direction='row' spacing={3} sx={{ p: 0.5 }}>
         {
-          ['degrees', 'subjects', 'groups', 'users', 'statistics'].map((item, index) => (
+          items.map((item, index) => (
             <Grid item xs={6} key={index}>
               <Button
                 sx={{ py: 8 }}
                 variant='outlined'
                 fullWidth
                 color='primary'
-                onClick={() => navigate(`/administration/${item}`)}
+                onClick={() => navigate(`/administration/${item.name}`)}
               >
-                {item}
+                {item.name} &nbsp;&nbsp; {item.icon && <item.icon />}
               </Button>
             </Grid>
           ))
