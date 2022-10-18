92f06cb596cb542f8303bb55b3b69d5d22b04946
scastd00 <scastd00@estudiantes.unileon.es>
2022-10-18 13:45:22 +0200

    Improved script to determine host IP

---
+++ determine_host_type.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# This script is used to export the necessary environment variables to support the automatic connection
+# to the home server. If this computer has an IP starting by 192.168.*.* then it is considered to be at home
+# and the connection to the home server is established directly. Otherwise, the connection to the home server
+# is by requesting the IP to the DuckDNS server.
+
+#######################################
+# Sets the environment variables to support the connection to the server
+# Arguments:
+#  None
+#######################################
+function main() {
+  # Check if the computer is at home
+  if [[ $(hostname -I) =~ ^192\.168\.0\..* ]]; then
+    # The computer is at home
+    echo 'You are at home'
+    echo 'VITE_PROD_HOST=192.168.0.100' >.env
+    echo 'VITE_DEV_HOST=127.0.0.1' >>.env
+    exit 0
+  fi
+
+  # The computer is away home
+  if [[ $(hostname -I) =~ ^192\.*\.*\.* ]]; then
+    # The computer is connected to the mobile network
+    echo 'You are connected to the mobile network'
+    echo 'VITE_PROD_HOST=rs-chat.duckdns.org' >.env
+    echo 'VITE_DEV_HOST=127.0.0.1' >>.env
+    exit 0
+  fi
+
+  # The computer is deployed in Vercel
+  echo 'You are deployed in Vercel'
+  echo 'VITE_PROD_HOST=rs-chat.duckdns.org' >.env
+  echo 'VITE_DEV_HOST=rs-chat.duckdns.org' >>.env
+}
+
+main "$@"
